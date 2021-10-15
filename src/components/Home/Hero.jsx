dbd5f08f31f8c2f467c8b94fde7fa9f672ac354f
omair5 <muhammadomair.235@gmail.com>
2021-10-15 22:42:35 +0500

    gatsby-plugin-image implemented / our favourite destination section completed

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -42,7 +42,8 @@ top: 0%;
 bottom: 0%;
 left: 0%;
 right: 0%;
-background-color: rgba(0,0,0,0.3);
+/* background-color: rgba(0,0,0,0.); */
+filter: brightness(50%);
 z-index:2;
 }
 `
--- src/components/Trips/index.jsx
+++ src/components/Trips/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { useStaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
+import { GoLocation } from "react-icons/go";
+import Button from '../Button';
 
 const Trips = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query TripsComponentData {
         allTripsJson {
             nodes {
@@ -20,25 +23,97 @@ const Trips = () => {
         }
       
     `)
-    const tripsData = data.allTripsJson.nodes
+  const tripsData = data.allTripsJson.nodes
 
 
-    return (
-        <>
-            this is trip component
-            {
-                tripsData.map((value) => (
-                    <div key={value.id}>
-                        <h1>{value.alt}</h1>
-                        <h1>{value.location}</h1>
-                        <h1>{value.id}</h1>
-                        <GatsbyImage image={getImage(value.img)} alt={value.alt} />
-                    </div>
-                ))
-            }
-
-        </>
-    );
+  return (
+    <MainContainer>
+      <MainHeading>OUR FAVOURITE DESTINATIONS</MainHeading>
+      <WrapperContainer>
+        {
+          tripsData.map((value) => (
+            <TripCard key={value.id} >
+              <LocationAndButtonContainer>
+                <Location>
+                  <LocationIcon />
+                  <h4>{value.location}</h4>
+                </Location>
+                <Button title='View Destination' />
+              </LocationAndButtonContainer>
+              <ImageComponent image={getImage(value.img)} alt={value.alt} objectFit={'fill'} />
+            </TripCard>
+          ))
+        }
+      </WrapperContainer>
+    </MainContainer>
+  );
 }
 
 export default Trips;
+
+const MainContainer = styled.div`
+min-height: 100vh;
+display: flex;
+flex-direction:column;
+justify-content: center;
+align-items: center;
+`
+const MainHeading = styled.h1`
+margin: 15px 0px;
+font-size: clamp(1.5rem,5vw,2rem);
+color:#F26A2E;
+
+`
+
+const WrapperContainer = styled.div`
+display: grid;
+grid-template-columns: repeat(4,1fr);
+grid-gap: 20px;
+padding: 1rem 2rem;
+justify-items: center;
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (max-width: 1200px) {
+  grid-template-columns: 1fr 1fr;
+
+}
+/* Medium devices (landscape tablets, 768px and up) */
+@media only screen and (max-width: 868px) {
+  grid-template-columns: 1fr;
+}
+`
+
+const TripCard = styled.div`
+position: relative;
+height: 500px;
+max-width: 100%;
+border-radius: 10px;
+`
+const ImageComponent = styled(GatsbyImage)`
+height: 100%;
+width:100%;
+object-fit: fill;
+border-radius: 10px;
+opacity: 0.9;
+transition: 0.5s ease;
+z-index:4;
+filter: brightness(70%);
+&:hover{
+  transition: cubic-bezier(0.075, 0.82, 0.165, 1);
+  filter: brightness(100%);
+}
+`
+const LocationAndButtonContainer = styled.div`
+position: absolute;
+bottom: 30px;
+left: 20px;
+z-index: 5;
+`
+const Location = styled.div`
+display: flex;
+color: white;
+letter-spacing: 1px;
+margin-bottom: 15px;
+`
+const LocationIcon = styled(GoLocation)`
+margin-right:7px
+`
